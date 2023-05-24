ca4bf716c373aee5c018f554432653833875e0bf
Agarwal, Anshika A <anshika.a.agarwal@accenture.com>
2023-05-24 16:27:18 +0200

    search results change

--- src/app/components/movie-search-results/movie-search-results.component.ts
+++ src/app/components/movie-search-results/movie-search-results.component.ts
@@ -1,53 +1,44 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { catchError, Subscription, throwError } from 'rxjs';
-import { Movie } from 'src/app/model/movie';
-import { MovieService } from 'src/app/service/movie.service';
-
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { catchError, Observable, Subscription, throwError } from "rxjs";
+import { Movie } from "src/app/model/movie";
+import { MovieService } from "src/app/service/movie.service";
 
 @Component({
-  selector: 'app-movie-search-results',
-  templateUrl: './movie-search-results.component.html',
-  styleUrls: ['./movie-search-results.component.css'],
+  selector: "app-movie-search-results",
+  templateUrl: "./movie-search-results.component.html",
+  styleUrls: ["./movie-search-results.component.css"],
 })
 export class MovieSearchResultsComponent implements OnInit, OnDestroy {
-  query: string = '';
-  movies: Movie[] | undefined;
-  errMsg: string = '';
-  movieSubscription = new Subscription();
-  queryParamSubscription = new Subscription();;
+  query: string = "";
+  errMsg: string = "";
+  queryParamSubscription = new Subscription();
+  movieSearchResults$: Observable<Movie[]> | undefined;
 
   constructor(
     private readonly route: ActivatedRoute,
-    private readonly movieService: MovieService
+    public movieService: MovieService
   ) {}
 
   ngOnInit(): void {
+    this.movieService.errMsg = "";
     this.queryParamSubscription = this.route.queryParams.subscribe((params) => {
-      this.query = params['query'];
+      this.query = params["query"];
       this.getSearchResults();
     });
   }
 
   getSearchResults() {
-    this.movieSubscription = this.movieService
-      .searchMovie(this.query)
-      .pipe(
-        catchError((err) => {
-          console.log('Handling error locally and rethrowing it...', err);
-          this.errMsg = err.error.Error;
-          return throwError(() => err);
-        })
-      )
-      .subscribe((data: any) => {
-        data.Response === 'True'
-          ? (this.movies = data.Search)
-          : (this.errMsg = data.Error);
-      });
+    this.movieSearchResults$ = this.movieService.searchMovie(this.query).pipe(
+      catchError((err) => {
+        console.log("Handling error locally and rethrowing it...", err);
+        this.errMsg = err.error.Error;
+        return throwError(() => err);
+      })
+    );
   }
 
   ngOnDestroy(): void {
-    this.movieSubscription.unsubscribe();
     this.queryParamSubscription.unsubscribe();
   }
 }
--- src/app/service/movie.service.ts
+++ src/app/service/movie.service.ts
@@ -1,18 +1,28 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
-import { Observable, pipe } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
 import { Movie, MovieDetail } from '../model/movie';
 
 @Injectable()
 export class MovieService {
 
   private readonly API_KEY = `6c3a2d45`;
-
+  errMsg: string = '';
   constructor(private http: HttpClient) { }
 
   searchMovie(searchQuery: string): Observable<Movie[]> {
     return this.http.get<Movie[]>(`https://omdbapi.com/?apikey=${this.API_KEY}&s=${searchQuery}`)
+    .pipe(
+      map((data: any) => {
+        if ( data.Response === 'True'){
+          return  data.Search;
+        } else {
+          this.errMsg = data.Error;
+          throwError(() => this.errMsg);
+        }
+      })
+    )
   }
 
   getMovieDetailsByID(imdbId: string): Observable<MovieDetail> {
